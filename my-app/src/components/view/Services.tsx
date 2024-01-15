fa77979a869ff58c2a26e757d7fda291ac74bc87
Rafi Ali <therafiali@gmail.com>
2024-01-15 16:13:36 +0500

    Add Services Section

--- my-app/src/components/view/Services.tsx
+++ my-app/src/components/view/Services.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+
 
 const Services = () => {
   return (
     <div>
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-      Our Services
-    </h2>
+
+      <div className='mt-10 mb-10   mx-auto'>
+        <h2 className=" text-center scroll-m-20  text-5xl font-bold tracking-normal first:mt-0 text-[#002060]">
+          Our Services
+        </h2>
+      </div>
+      <div className='max-w-3xl mx-auto'> 
+        <p className="leading-7 [&:not(:first-child)]:mt-6 text-center text-[#3a3a3a]">
+          Your organization has its own unique challenges and characteristics. We understand that better and we serve as an extension of your team. Make the most out of your technology investments with our service offerings – because bringing people and technology together is what we do best. Explore our managed and consulting services below and learn how we can help you to achieve better business outcomes.
+        </p>
+
+      </div>
 
     </div>
+
   )
 }
 
