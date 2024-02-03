dabee32e5dac4c445d240ebd4eae2e568fcd49c6
Rafi Ali <rafialiofficial810@gmail.com>
2024-02-03 13:47:06 +0500

    Added different animation

--- my-app/src/components/view/Services_Items.tsx
+++ my-app/src/components/view/Services_Items.tsx
@@ -36,56 +36,6 @@ const Services_Items = () => {
                                 <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
                             </div>
                         </div>
-                        <div className='flex flex-1 h-full items-start justify-center gap-x-3  p-6 bg-gray-200 border rounded-sm'>
-                            <div className='relative flex flex-col item-center justify-center '>
-                                <p className='bg-gray-50 rounded-full h-24 w-24 '></p>
-                                <div className='absolute m-auto left-3'>
-
-                                    <Image src={"/insurance.png"} alt='Cloud' width={70} height={70} />
-                                </div>
-                            </div>
-                            <div className=''>
-
-                                <p className="text-sm font-bold tracking-wider leading-relaxed text-gray-400">
-                                    SOFTWARE
-                                </p>
-                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left mt-2 mb-4">
-                                    Cloud Computing Services 1
-                                </h3>
-                                <div className='max-w-sm text-center mx-auto text-[#3a3a3a] leading-relaxed'>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                </div>
-                                <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
-                            </div>
-                        </div>
-                        <div className='flex flex-1 h-full items-start justify-center gap-x-3  p-6 bg-gray-200 border rounded-sm'>
-                            <div className='relative flex flex-col item-center justify-center '>
-                                <p className='bg-gray-50 rounded-full h-24 w-24 '></p>
-                                <div className='absolute m-auto left-3'>
-
-                                    <Image src={"/insurance.png"} alt='Cloud' width={70} height={70} />
-                                </div>
-                            </div>
-                            <div className=''>
-
-                                <p className="text-sm font-bold tracking-wider leading-relaxed text-gray-400">
-                                    SOFTWARE
-                                </p>
-                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left mt-2 mb-4">
-                                    Cloud Computing Services 1
-                                </h3>
-                                <div className='max-w-sm text-center mx-auto text-[#3a3a3a] leading-relaxed'>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                    <p className=''>Cloud Infrastructure Setup and Management</p>
-                                </div>
-                                <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
-                            </div>
-                        </div>
                     </div>
                 </Animation>
                 <Animation delay={1} direction={'right'}>
@@ -117,6 +67,13 @@ const Services_Items = () => {
                                 <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
                             </div>
                         </div>
+                    </div>
+                </Animation>
+            </div>
+            <div className='flex justify-between items-center mt-16 '>
+                <Animation delay={2} direction={'left'}>
+                    <div className='space-y-8'>
+
                         <div className='flex flex-1 h-full items-start justify-center gap-x-3  p-6 bg-gray-200 border rounded-sm'>
                             <div className='relative flex flex-col item-center justify-center '>
                                 <p className='bg-gray-50 rounded-full h-24 w-24 '></p>
@@ -142,6 +99,75 @@ const Services_Items = () => {
                                 <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
                             </div>
                         </div>
+                    </div>
+                </Animation>
+                <Animation delay={2} direction={'right'}>
+
+                    <div className='space-y-8'>
+
+                        <div className='flex flex-1 h-full items-start justify-center gap-x-3  p-6 bg-gray-200 border rounded-sm'>
+                            <div className='relative flex flex-col item-center justify-center '>
+                                <p className='bg-gray-50 rounded-full h-24 w-24 '></p>
+                                <div className='absolute m-auto left-3'>
+
+                                    <Image src={"/insurance.png"} alt='Cloud' width={70} height={70} />
+                                </div>
+                            </div>
+                            <div className=''>
+
+                                <p className="text-sm font-bold tracking-wider leading-relaxed text-gray-400">
+                                    SOFTWARE
+                                </p>
+                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left mt-2 mb-4">
+                                    Cloud Computing Services 1
+                                </h3>
+                                <div className='max-w-sm text-center mx-auto text-[#3a3a3a] leading-relaxed'>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                </div>
+                                <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
+                            </div>
+                        </div>
+                    </div>
+                </Animation>
+            </div>
+            <div className='flex justify-between items-center mt-16 '>
+                <Animation delay={2} direction={'left'}>
+                    <div className='space-y-8'>
+
+                        <div className='flex flex-1 h-full items-start justify-center gap-x-3  p-6 bg-gray-200 border rounded-sm'>
+                            <div className='relative flex flex-col item-center justify-center '>
+                                <p className='bg-gray-50 rounded-full h-24 w-24 '></p>
+                                <div className='absolute m-auto left-3'>
+
+                                    <Image src={"/insurance.png"} alt='Cloud' width={70} height={70} />
+                                </div>
+                            </div>
+                            <div className=''>
+
+                                <p className="text-sm font-bold tracking-wider leading-relaxed text-gray-400">
+                                    SOFTWARE
+                                </p>
+                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left mt-2 mb-4">
+                                    Cloud Computing Services 1
+                                </h3>
+                                <div className='max-w-sm text-center mx-auto text-[#3a3a3a] leading-relaxed'>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                    <p className=''>Cloud Infrastructure Setup and Management</p>
+                                </div>
+                                <Button className='text-center mx-auto mt-2' variant="outline">LEARN MORE</Button>
+                            </div>
+                        </div>
+                    </div>
+                </Animation>
+                <Animation delay={2} direction={'right'}>
+
+                    <div className='space-y-8'>
+
                         <div className='flex flex-1 h-full items-start justify-center gap-x-3  p-6 bg-gray-200 border rounded-sm'>
                             <div className='relative flex flex-col item-center justify-center '>
                                 <p className='bg-gray-50 rounded-full h-24 w-24 '></p>
